529e270c9122f50e4e0ff23160fb17f0e00a6203
alexiapana <alexia.pana@student.unitbv.ro>
2023-03-29 22:09:06 +0300

    am creat baza de date in memorie si cele 4 endpoint-uri !!!de revazut metoda DeleteEmplyee

---
+++ tema1/tema1/Controllers/EmployeeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using tema1.Model;
+
+namespace tema1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public static List<Employee> employees = new()
+        {
+            new Employee{ Id=1, FirstName="Maria", LastName="Popescu", Department="Marketing"}
+            , new Employee{ Id=2, FirstName="Andrei", LastName="Ionescu", Department="Resurse umane"}
+        };
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            return Ok(employees);
+        }
+        [HttpGet ("{id}")]
+        public IActionResult GetEmployee(int id)
+        {
+            var employee = employees.Find(x =>x.Id == id);
+            if(employee == null)
+            {
+                return NotFound("Employee not found");
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateEmployeeData(Employee employee)
+        {
+            var employeeInList = employees.Find(x => x.Id == employee.Id);
+            if(employee == null) {
+                return NotFound("Invalid Id!");
+            }
+            employeeInList.FirstName = employee.FirstName;
+            employeeInList.LastName = employee.LastName;
+            employeeInList.Department = employee.Department;
+            return Ok(employeeInList);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteEmployee(int id)
+        {
+            var employee = employees.Find(x => x.Id == x.Id);
+            if (employee == null)
+            {
+                return NotFound("invalid Id!");
+            }
+            employees.Remove(employee);
+            return Ok(employee);
+        }
+    }
+}
---
+++ tema1/tema1/Model/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace tema1.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+    }
+}
